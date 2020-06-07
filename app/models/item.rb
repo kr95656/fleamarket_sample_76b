0c1dff67982f9766d4e211d26ff8c5563cf7c5c4
shnpess <ann2lax@gmail.com>
2020-06-07 12:12:42 +0900

    :feat shipping_prefectureモデルを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :item_condition
   belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :shipping_prefecture
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_030451) do
+ActiveRecord::Schema.define(version: 2020_06_07_031004) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 2020_06_07_030451) do
   end
 
   create_table "postage_payers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shipping_prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
