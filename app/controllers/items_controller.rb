f663c55ebd18b5d007fb62f533267a559ae46f40
shnpess <ann2lax@gmail.com>
2020-06-07 11:14:30 +0900

    feat:Items ControllerのCreate部分を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+    @item.images.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render ("items/new")
+    end
+    
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :category_id, :brand, :item_condition_id, :postage_payer_id, :shipping_prefecture_id, :shipping_day_id, :price, images_attributes: [:url])
+  end
 end
