1ea62e8765131fc647a538747b7b38715a132eda
shnpess <ann2lax@gmail.com>
2020-06-07 14:05:47 +0900

    feat: 複数枚の画像投稿機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    
+    @item.images.new
   end
 
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to :category
+  has_many :images
+  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :item_condition
   belongs_to_active_hash :postage_payer
