fd553d9fd87eb60313f85068cecc2a940a77ff08
kr95656 <katmsg8239@gmail.com>
2020-06-09 20:24:56 +0900

    Change description src to url

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -3,8 +3,8 @@ $(document).on('turbolinks:load', ()=> {
   const buildFileField = (index)=> {
     const html = `<div data-index="${index}" class="js-file_group">
                     <input class="js-file" type="file"
-                    name="item[images_attributes][${index}][src]"
-                    id="item_images_attributes_${index}_src"><br>
+                    name="item[images_attributes][${index}][url]"
+                    id="item_images_attributes_${index}_url"><br>
                   </div>`;
     return html;
   }
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ApplicationRecord
-  mount_uploader :src, ImageUploader
+  mount_uploader :url, ImageUploader
   belongs_to :item
 end
