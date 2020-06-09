030ce3e532e3dab5fe2bc1866f89189f96b9ef66
kr95656 <katmsg8239@gmail.com>
2020-06-09 20:11:34 +0900

    Introduce S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,20 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-#     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-#     region: 'ap-northeast-1'
-#   }
-
-#   config.fog_directory  = 'nakasu-76b'
-#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/nakasu-76b'
-# end
+CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'f76.team.production'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/f76.team.production'
+  end
+end
