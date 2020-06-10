0191cc987901836404af3d6021b438eb11e2f3d0
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-06-10 10:42:44 +0900

    ancestry導入_カテゴリ一覧追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
-  has_many :items 
+  has_many :items
+  has_ancestry
+
+  validates :name, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_055654) do
+ActiveRecord::Schema.define(version: 2020_06_09_110723) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
