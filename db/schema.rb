6adc398876904d18e4ca109eb70b287020f9e07c
yuya0125 <62539180+yuya0125@users.noreply.github.com>
2020-06-11 16:53:53 +0900

    【サーバーサイド】新規登録・ログイン機能　GEMファイルの追記

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,48 @@
 
 ActiveRecord::Schema.define(version: 2020_06_11_030508) do
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "url", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
+
+  create_table "item_conditions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "name", null: false
+    t.text "introduction", null: false
+    t.bigint "category_id", null: false
+    t.string "brand"
+    t.string "item_condition_id", null: false
+    t.string "postage_payer_id", null: false
+    t.string "shipping_prefecture_id", null: false
+    t.string "shipping_day_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "trading_status", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
+  create_table "postage_payers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -24,6 +66,11 @@ ActiveRecord::Schema.define(version: 2020_06_11_030508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "shipping_days", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shipping_destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -43,6 +90,11 @@ ActiveRecord::Schema.define(version: 2020_06_11_030508) do
     t.index ["user_id"], name: "index_shipping_destinations_on_user_id"
   end
 
+  create_table "shipping_prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -56,6 +108,8 @@ ActiveRecord::Schema.define(version: 2020_06_11_030508) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "items"
+  add_foreign_key "items", "categories"
   add_foreign_key "profiles", "users"
   add_foreign_key "shipping_destinations", "users"
 end
