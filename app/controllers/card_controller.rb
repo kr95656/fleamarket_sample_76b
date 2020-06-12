25a5c7eb581b071375c0b54cb574e6bcba9e39c0
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-06-12 12:11:18 +0900

    controller編集

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,7 +1,51 @@
 class CardController < ApplicationController
+
+  require "payjp"
+
   def new
+    card = Card.where(user_id: current_user.id)
+    redirect_to action: "show" if card.exists?
+  end
+
+  def pay
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    if params['payjp-token'].blank?
+      redirect_to credit_card_path(current_user.id), alert: "クレジットカードが登録できませんでした。"
+    else
+      customer = Payjp::Customer.create(
+      email: current_user.email,
+      card: params['payjp-token'],
+      metadata: {user_id: current_user.id}
+      ) 
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
+  end
+
+  def delete #PayjpとCardデータベースを削除します
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "new", alert: "削除できませんでした。"
   end
 
   def show
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
   end
 end
