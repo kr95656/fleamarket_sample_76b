cc592b94d82a232aedff6b889fad0fe7741de2d7
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-06-12 12:34:30 +0900

    routes編集

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,12 @@ Rails.application.routes.draw do
   end
 
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
