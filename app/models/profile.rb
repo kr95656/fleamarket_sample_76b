063e790fe698c920fc962926fe0a06b4f9dc9c17
yuya0125 <62539180+yuya0125@users.noreply.github.com>
2020-06-13 13:15:20 +0900

    【サーバーサイド】新規登録バリデーション修正（本人確認・商品情報入力画面）

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
-  validates :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day, :user, presence: true
-
+  # validates :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day, :user, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :user, presence: true
 end
--- app/models/shipping_destination.rb
+++ app/models/shipping_destination.rb
@@ -3,9 +3,18 @@ class ShippingDestination < ApplicationRecord
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  # validates :destination_first_name, :destination_last_name, :destination_first_name_kana, :destination_last_name_kana, :post_code, :prefecture_code, :city, :address, :building, :phone_number, :user,
+  validates :destination_first_name, presence: true
+  validates :destination_last_name, presence: true
+  validates :destination_first_name_kana, presence: true
+  validates :destination_last_name_kana, presence: true
+  validates :post_code, presence: true
+  validates :prefecture_code, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :building, presence: true
+  validates :phone_number, presence: true
+  validates :user, presence: true
 
-
-
-  validates :destination_first_name, :destination_last_name, :destination_first_name_kana, :destination_last_name_kana, :post_code, :prefecture_code, :city, :address, :building, :phone_number, :user, presence: true
 
 end
