82b84b9d5d8cc65769dab96cccaacc14830a3e32
yuya0125 <62539180+yuya0125@users.noreply.github.com>
2020-06-16 13:37:12 +0900

    【サーバーサイド】新規登録　メールアドレス　バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, presence: true, length: { minimum: 7, maximum: 32}
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
   has_one :profile,dependent: :destroy
   has_one :shipping_destination, dependent: :destroy
   has_one :credit_card, dependent: :destroy
