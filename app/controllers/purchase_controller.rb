76a46793c0cf66fb05c2abc39662b7871a06c185
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-06-23 15:37:53 +0900

    クレジットカード情報の修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -19,10 +19,11 @@ class PurchaseController < ApplicationController
   end
 
   def pay
+    @item = Item.find(params[:item_id])
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => 13500,
+    :amount => @item.price,
     :customer => card.customer_id,
     :currency => 'jpy',
   )
